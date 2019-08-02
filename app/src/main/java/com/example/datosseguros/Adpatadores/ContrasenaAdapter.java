d2ddcc45fb005169bc6aae85785f40211dd1c88e
Prueba <hectorsalomonchirinos@gmial.com>
2019-08-02 17:00:40 -0400

    1.0.0.0

---
+++ app/src/main/java/com/example/datosseguros/Adpatadores/ContrasenaAdapter.java
@@ -0,0 +1,67 @@
+package com.example.datosseguros.Adpatadores;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.datosseguros.Constructores.ContrasenaConstructor;
+import com.example.datosseguros.R;
+
+import java.util.ArrayList;
+
+public class ContrasenaAdapter extends RecyclerView.Adapter<ContrasenaAdapter.ViewHolderContrasena> {
+
+    private ArrayList<ContrasenaConstructor> listContrasena;
+    private Context mCtx;
+
+    public ContrasenaAdapter (ArrayList<ContrasenaConstructor> listContrasena, Context mCtx) {
+        this.listContrasena = listContrasena;
+        this.mCtx = mCtx;
+    }
+
+    @NonNull
+    @Override
+    public ContrasenaAdapter.ViewHolderContrasena onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.cardview_contrasena, null, false);
+
+        return new ViewHolderContrasena(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ContrasenaAdapter.ViewHolderContrasena viewHolderContrasena, int i) {
+
+        /*viewHolderContrasena.servicio.setText(listContrasena.get(i).getServicio());
+        viewHolderContrasena.usuario.setText(listContrasena.get(i).getUsuario());
+        viewHolderContrasena.contrasena.setText(listContrasena.get(i).getContrasena());
+        viewHolderContrasena.vencimiento.setText(listContrasena.get(i).getVencimiento());*/
+
+        viewHolderContrasena.servicio.setText("PRueba");
+        viewHolderContrasena.usuario.setText("Linea 2");
+        viewHolderContrasena.contrasena.setText("Linea 3");
+        viewHolderContrasena.vencimiento.setText("Linea 4");
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listContrasena.size();
+    }
+
+    public class ViewHolderContrasena extends RecyclerView.ViewHolder {
+
+        TextView servicio, usuario, contrasena, vencimiento;
+
+        public ViewHolderContrasena(@NonNull View itemView) {
+            super(itemView);
+
+            servicio = (TextView) itemView.findViewById(R.id.tvServicio);
+            usuario = (TextView) itemView.findViewById(R.id.tvUsuario);
+            contrasena = (TextView) itemView.findViewById(R.id.tvContrasena);
+            vencimiento = (TextView) itemView.findViewById(R.id.tvVencimiento);
+        }
+    }
+}
--- app/src/main/java/com/example/datosseguros/MainActivity.java
+++ app/src/main/java/com/example/datosseguros/MainActivity.java
@@ -5,12 +5,23 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import com.example.datosseguros.Adpatadores.ContrasenaAdapter;
+import com.example.datosseguros.Constructores.ContrasenaConstructor;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mTextMessage;
     private FloatingActionButton fabAdd;
+    private RecyclerView recycler;
+    private ContrasenaAdapter adapter;
+    private ArrayList<ContrasenaConstructor> listContrasena;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -19,15 +30,16 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_contrasena:
-                    //mTextMessage.setText(R.string.title_home);
+                    fabAdd.setImageResource(R.drawable.ic_add_contrasena);
                     return true;
                 case R.id.navigation_cuentas:
-                    //mTextMessage.setText(R.string.title_dashboard);
+                    fabAdd.setImageResource(R.drawable.ic_add_banco);
                     return true;
                 case R.id.navigation_tarjetas:
-                    //mTextMessage.setText(R.string.title_notifications);
+                    fabAdd.setImageResource(R.drawable.ic_add_card);
                     return true;
                 case R.id.navigation_notas:
+                    fabAdd.setImageResource(R.drawable.ic_add_nota);
                     return true;
             }
             return false;
@@ -38,9 +50,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+
+
+        recycler = (RecyclerView) findViewById(R.id.recycler);
+        recycler.setLayoutManager(new LinearLayoutManager(this));
+        recycler.setHasFixedSize(true);
+        listContrasena = new ArrayList<>();
+
+        testContrasena();
+        adapter = new ContrasenaAdapter(listContrasena, this);
+        recycler.setAdapter(adapter);
+
+        fabAdd = (FloatingActionButton) findViewById(R.id.fab_agregar);
+
+    }
+
+    private void testContrasena() {
+        listContrasena.add(new ContrasenaConstructor("Digitel", "Hector", "1234", 12));
+        listContrasena.add(new ContrasenaConstructor("Google", "Salomon", "5678", 60));
     }
 
 }
