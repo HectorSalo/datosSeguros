e104b6f6e11a97d48e596be8d4ce815f8007aad5
Prueba <hectorsalomonchirinos@gmial.com>
2019-08-13 16:53:23 -0400

    1.0.0.12

---
+++ app/src/main/java/com/example/datosseguros/InicSesionActivity.java
@@ -0,0 +1,42 @@
+package com.example.datosseguros;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class InicSesionActivity extends AppCompatActivity {
+
+    private EditText usuario, contrasena;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inic_sesion);
+
+        usuario = (EditText) findViewById(R.id.etInicSesionUsuario);
+        contrasena = (EditText) findViewById(R.id.etInicSesionContrasena);
+        TextView registrate = (TextView) findViewById(R.id.tvRegistrate);
+        TextView cuentaGoogle = (TextView) findViewById(R.id.tvcuentaGoogle);
+
+        registrate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Registro", Toast.LENGTH_SHORT).show();
+            }
+        });
+        
+        Button inicSesion = (Button) findViewById(R.id.buttonInicSesion);
+        inicSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(InicSesionActivity.this, MainActivity.class));
+            }
+        });
+    }
+}
