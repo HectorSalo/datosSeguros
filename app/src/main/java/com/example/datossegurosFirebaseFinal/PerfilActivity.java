70e10483ae885fbdd4947754a155eb8a7b7cd47a
Prueba <hectorsalomonchirinos@gmial.com>
2019-09-12 17:52:03 -0400

    2.0.5

--- app/src/main/java/com/example/datossegurosFirebaseFinal/PerfilActivity.java
+++ app/src/main/java/com/example/datossegurosFirebaseFinal/PerfilActivity.java
@@ -10,6 +10,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -33,7 +35,7 @@ public class PerfilActivity extends AppCompatActivity {
         buttonPass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //actualizarPass();
+                actualizarPass();
             }
         });
     }
@@ -46,18 +48,74 @@ public class PerfilActivity extends AppCompatActivity {
     public void actualizarEmail() {
         LinearLayout layout = new LinearLayout(this);
         layout.setOrientation(LinearLayout.VERTICAL);
+
         final EditText email = new EditText(this);
-        EditText repetirEmail = new EditText(this);
+        final EditText repetirEmail = new EditText(this);
+        email.setHint("Ingrese nueva dirección de correo");
+        repetirEmail.setHint("Repita la dirección de correo");
+
+        layout.addView(email);
+        layout.addView(repetirEmail);
 
         androidx.appcompat.app.AlertDialog.Builder dialog = new AlertDialog.Builder(this);
-        dialog.setMessage("Ingrese correo electrónico")
-                .setTitle("Cambio de contraseña")
-                .setView(email)
-                .setView(repetirEmail)
+        dialog.setTitle("Cambio de Correo")
+                .setView(layout)
                 .setPositiveButton("Cambiar", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         String correo = email.getText().toString();
+                        String repetirCorreo = repetirEmail.getText().toString();
+
+                        if (!correo.isEmpty()) {
+                            if (correo.contains("@")) {
+                                if (correo.equals(repetirCorreo)) {
+                                    dialog.dismiss();
+                                    Toast.makeText(getApplicationContext(), "True", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    email.setError("Las contraseñas deben coincidir");
+                                }
+                            } else {
+                                email.setError("Formato incorrecto para correo");
+
+                            }
+                        } else {
+                            email.setError("El campo no puede estar vacío");
+
+                        }
+
+
+                    }
+                })
+                .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //dialog.dismiss();
+
+                    }
+                })
+                .setCancelable(false)
+                .show();
+    }
+
+    public void actualizarPass() {
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+
+        final EditText pass = new EditText(this);
+        EditText repetirPass = new EditText(this);
+        pass.setHint("Ingrese nueva contraseña");
+        repetirPass.setHint("Repita la contraseña");
+
+        layout.addView(pass);
+        layout.addView(repetirPass);
+
+        androidx.appcompat.app.AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Cambio de contraseña")
+                .setView(layout)
+                .setPositiveButton("Cambiar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String passw = pass.getText().toString();
                         //enviarCorreo(correo);
                         dialog.dismiss();
                     }
