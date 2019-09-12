b417d8c05e449bff362f3aa2ebbda2d763d8696d
Prueba <hectorsalomonchirinos@gmial.com>
2019-09-12 15:26:47 -0400

    2.0.3

--- app/src/main/java/com/example/datossegurosFirebaseFinal/PerfilActivity.java
+++ app/src/main/java/com/example/datossegurosFirebaseFinal/PerfilActivity.java
@@ -1,11 +1,15 @@
 package com.example.datossegurosFirebaseFinal;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -15,10 +19,56 @@ public class PerfilActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
+
+        Button buttonEmail = (Button) findViewById(R.id.buttonUpdateEmail);
+        Button buttonPass = (Button) findViewById(R.id.buttonUpdatePass);
+
+        buttonEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                actualizarEmail();
+            }
+        });
+
+        buttonPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //actualizarPass();
+            }
+        });
     }
 
     @Override
     public void onBackPressed() {
         startActivity(new Intent(getApplicationContext(), MainActivity.class));
+    }
+
+    public void actualizarEmail() {
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        final EditText email = new EditText(this);
+        EditText repetirEmail = new EditText(this);
+
+        androidx.appcompat.app.AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setMessage("Ingrese correo electrónico")
+                .setTitle("Cambio de contraseña")
+                .setView(email)
+                .setView(repetirEmail)
+                .setPositiveButton("Cambiar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String correo = email.getText().toString();
+                        //enviarCorreo(correo);
+                        dialog.dismiss();
+                    }
+                })
+                .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .setCancelable(false)
+                .show();
     }
 }
