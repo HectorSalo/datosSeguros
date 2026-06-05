cd0380d2a8c5ac10c49084173c4c4b00e859332d
Hector PC <hchirinos@ubiipagos.com>
2026-06-05 11:14:01 -0400

    chore: route SharedPref PIN reset through PinStorage

--- app/src/main/java/com/skysam/datossegurosFirebaseFinal/database/sharedPreference/SharedPref.kt
+++ app/src/main/java/com/skysam/datossegurosFirebaseFinal/database/sharedPreference/SharedPref.kt
@@ -39,7 +39,7 @@ object SharedPref {
         val editor = getInstance().edit()
         editor.putString(Constants.PREFERENCE_TIPO_BLOQUEO, Constants.PREFERENCE_SIN_BLOQUEO)
         editor.putString(Constants.PREFERENCE_TEMA, Constants.PREFERENCE_AMARILLO)
-        editor.putString(Constants.PREFERENCE_PIN_RESPALDO, "0000")
         editor.apply()
+        PinStorage.reset()
     }
 }
